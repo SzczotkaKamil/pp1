414a289aeb7130cecf624b8dd0fe1506538d7abc
Kamil Szczotka <reindonyt@gmail.com>
2019-10-24 16:15:05 +0200

    a

---
+++ 01-TypesAndVariables/During Class/123.py
@@ -0,0 +1,20 @@
+'''
+Obliczanie pola powierzchni i obwodu koła o zadanym promieniu
+'''
+import math
+# ustal promień koła i PI
+# ... tu wpisz instrukcje programu ...
+pi =  math.pi
+r = 5
+
+# oblicz pole i obwód
+# ... tu wpisz instrukcje programu ...
+polePowierzchni = pi*r**2
+obwodKola = 2*pi*r
+# wyświetl rezultaty
+# ... tu wpisz instrukcje programu ...
+# Pole koła o promieniu ... wynosi ...
+print (f"Pole koła o promieniu {r} wynosi {polePowierzchni}")
+
+# Obwód koła o promieniu ... wynosi ...
+print (f"Obwód koła o promieniu {r} wynosi {obwodKola}")
